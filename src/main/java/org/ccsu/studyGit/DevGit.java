f7d367007db2da92488e9a61690558766ab4da75
Masker <1262917629@qq.com>
2022-05-09 13:55:25 +0800

    [add]:dev 分支提交

---
+++ src/main/java/org/ccsu/studyGit/DevGit.java
@@ -0,0 +1,11 @@
+package org.ccsu.studyGit;
+
+/**
+ * @author Haixin Wu
+ * @since 1.0
+ */
+public class DevGit {
+    public static void main(String[] args) {
+        System.out.println("Dev");
+    }
+}
