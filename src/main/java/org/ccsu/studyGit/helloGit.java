5b2b47d9f532d0fa5fdb29043005b463a6957f4d
Masker <1262917629@qq.com>
2022-05-09 13:49:04 +0800

    [update]:master 分支提交

--- src/main/java/org/ccsu/studyGit/helloGit.java
+++ src/main/java/org/ccsu/studyGit/helloGit.java
@@ -3,15 +3,7 @@ package org.ccsu.studyGit;
 public class helloGit {
 
 	public static void main(String[] args) {
-		System.out.println("hello Git");
-		System.out.println("创建分支");
-		System.out.println("更新");
-		System.out.println("更新");
-		System.out.println("更新");
-		System.out.println("更新");
-		System.out.println("更新");
-		System.out.println("更新");
-		System.out.println("更新");
+		System.out.println("master");
 	}
 
 }
